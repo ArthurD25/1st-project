4db3652f27d796ff6116dc7f1365e873c1aa817f
ArthurD25 <arthuro.oneal@gmail.com>
2021-05-01 16:32:54 -0400

    Added variables destination, entertainment, restaurant, and transportation.

---
+++ dayGenerator.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let destination = ["Hawaii", "Paris", "Dubai", "Bora Bora", "Japan"];
+let entertainment = ["Tour", "Dancing", "Fair", "Cruise", "Concert" ];
+let restaurant = ["Chinese Restaurant", "Italian Restaurant", "Steak Restaurant", "Greek Restaurant", "Sushi Restaurant"];
+let transportation = ["Private Jet", "Boat trip", "Helicopter Rides", "Limo Ride"];
